3d9b868128e28fc3ff9d3fbfc0e93d535c8ef333
Eli Chan <eli.chan@intertwine.io>
2015-12-27 03:51:40 -0600

    add ProblemConnection class

--- data/data_process.py
+++ data/data_process.py
@@ -18,6 +18,95 @@ class AsymmetricConnection(Exception):
                                                              not_connected_type_b)
         self.message = msg
 
+class InvalidConnectionType(Exception):
+    def __init__(self, connection_type):
+        msg = 'Connection type "{!s}" is not valid.'.format(connection_type)
+        self.message = msg
+
+class CircularConnection(Exception):
+    def __init__(self, problem):
+        msg = '"{!r}" cannot be connected to itself.'.format(problem)
+        self.message = msg
+
+
+class ProblemConnectionRating(object):
+    '''Base class for problem connection ratings
+
+    Problem connection ratings are input by users and are scoped by
+    geo, organization, AND problem context. It is perfectly valid
+    for the same user to provide a different rating of A -> B from
+    the context of problem A vs. problem B because the perceived
+    importance of B as an impact of A may be quite different from
+    the perceived importance of A as a driver of B.'''
+    pass
+
+class ProblemConnection(object):
+    '''Base class for problem connections
+
+    There are four ways in which problems can be connected:
+
+                               broader
+                                  |
+                    drivers -> problem -> impacts
+                                  |
+                               narrower
+
+    Drivers/impacts are causal type connections while broader/narrower
+    are scope type connections. In causal connections, the driver is
+    always listed first, while in scope connections, the broader is
+    always listed first. A problem connection is uniquely definied by
+    its type, first problem, and second problem.'''
+
+    # keeps track of all instances
+    _registry = {}
+
+    def __new__(cls, connection_type, problem_a, problem_b):
+        """Create a new problem connection if it doesn't already exist
+
+        Checks if a problem connection already exists in the _registry.
+        If it does, return it with new=False. If it doesn't, create it
+        and return it with new=True. The "new" attribute is used by
+        __init__ to distinguish between new vs. existing connections.
+        """
+        # todo: make connection_type an Enum
+        if connection_type not in ('causal', 'scope'):
+            raise InvalidConnectionType(connection_type)
+        if problem_a == problem_b:
+            raise CircularConnection(problem_a)
+        key = (connection_type, problem_a, problem_b)
+        problem_connection = ProblemConnection._registry.get(key, None)
+        if problem_connection:
+            problem_connection.new = False
+            return problem_connection
+        else:
+            # todo?: instead of calling object's __new__, call parent's __new__
+            problem_connection = object.__new__(cls, connection_type,
+                                                problem_a, problem_b)
+            problem_connection.new = True
+            ProblemConnection._registry[key] = problem_connection
+            return problem_connection
+
+    def __init__(self, connection_type, problem_a, problem_b):
+        if self.new:
+            self.connection_type = connection_type
+            self.problem_a = problem_a
+            self.problem_b = problem_b
+
+        del self.new
+
+    def __repr__(self):
+        cname = self.__class__.__name__
+        return '<{!s}: {!s} {!r}-{!r}>'.format(cname, self.connection_type,
+                                               self.problem_a, self.problem_b)
+
+    def __str__(self):
+        if self.connection_type == 'causal':
+            s = '<{!s} -> {!s}>'.format(self.problem_a.name, self.problem_b.name)
+        elif self.connection_type == 'scope':
+            indent = ' ' * (len(self.problem_a.name)/2 + 1)
+            s = '<{!s}\n{!s}|\n{!s}>'.format(self.problem_a.name, indent, self.problem_b.name)
+        return s
+
 class Problem(object):
     '''Base class for problems
 
@@ -45,7 +134,7 @@ class Problem(object):
             problem.new = False
             return problem
         else:
-            # todo: instead of calling object's __new__, call parent's __new__
+            # todo?: instead of calling object's __new__, call parent's __new__
             problem = object.__new__(cls, name, definition=None, definition_url=None, images=[])
             problem.new = True
             Problem._registry[key] = problem
@@ -60,22 +149,7 @@ class Problem(object):
         the problem already exists in the _registry, each field is set
         only if the corresponding input value is None or [] for lists. A
         FieldCollision exception is raised if the existing field and
-        the input both have values (or len>0 for lists).
-
-        The problem connection types are related to problems as follows:
-
-                                   broader
-                                      |
-                        drivers -> problem -> impacts
-                                      |
-                                   narrower
-
-        Problem connection ratings are input by users and are scoped by
-        geo, organization, AND problem context. It is perfectly valid
-        for the same user to provide a different rating of A -> B from
-        the context of problem A vs. problem B because the perceived
-        importance of B as an impact of A may be quite different from
-        the perceived importance of A as a driver of B.'''
+        the input both have values (or len>0 for lists).'''
 
         if self.new:
             self.name = name.title()
@@ -110,14 +184,11 @@ class Problem(object):
                 raise MissingRequiredField(field='adjacent_problem_name', classname='Problem')
             adjacent_problem_name = adjacent_problem_name.title()
             adjacent_problem = Problem(name=adjacent_problem_name)
-            if adjacent_problem not in self.drivers:
-                # print 'Adding {!r} as a driver of {!r}'.format(adjacent_problem, self)
-                self.drivers.append(adjacent_problem)
-                adjacent_problem.impacts.append(self)
-            # else:
-            #     print '{!r} is already a driver of {!r}'.format(adjacent_problem, self)
-
-            # todo: add problem connections and ratings
+            problem_connection = ProblemConnection('causal', adjacent_problem, self)
+            if problem_connection not in self.drivers:
+                self.drivers.append(problem_connection)
+                adjacent_problem.impacts.append(problem_connection)
+            # todo: add problem connection ratings
 
         for problem_connection_data in impacts:
             adjacent_problem_name = problem_connection_data.get('adjacent_problem', None)
@@ -125,14 +196,11 @@ class Problem(object):
                 raise MissingRequiredField(field='adjacent_problem_name', classname='Problem')
             adjacent_problem_name = adjacent_problem_name.title()
             adjacent_problem = Problem(name=adjacent_problem_name)
-            if adjacent_problem not in self.impacts:
-                # print 'Adding {!r} as an impact of {!r}'.format(adjacent_problem, self)
-                self.impacts.append(adjacent_problem)
-                adjacent_problem.drivers.append(self)
-            # else:
-            #     print '{!r} is already an impact of {!r}'.format(adjacent_problem, self)
-
-            # todo: add problem connections and ratings
+            problem_connection = ProblemConnection('causal', self, adjacent_problem)
+            if problem_connection not in self.impacts:
+                self.impacts.append(problem_connection)
+                adjacent_problem.drivers.append(problem_connection)
+            # todo: add problem connection ratings
 
         # todo: add broader connections
         # todo: add narrower connections
@@ -149,17 +217,17 @@ class Problem(object):
         for image in self.images:
             s += '    {!s}\n'.format(image)
         s += 'drivers:\n'
-        for problem in self.drivers:
-            s += '    {!s}\n'.format(problem.name)
+        for problem_connection in self.drivers:
+            s += '    {!s}\n'.format(problem_connection.problem_a.name)
         s += 'impacts:\n'
-        for problem in self.impacts:
-            s += '    {!s}\n'.format(problem.name)
+        for problem_connection in self.impacts:
+            s += '    {!s}\n'.format(problem_connection.problem_b.name)
         s += 'broader:\n'
-        for problem in self.broader:
-            s += '    {!s}\n'.format(problem.name)
+        for problem_connection in self.broader:
+            s += '    {!s}\n'.format(problem_connection.problem_a.name)
         s += 'narrower:\n'
-        for problem in self.narrower:
-            s += '    {!s}\n'.format(problem.name)
+        for problem_connection in self.narrower:
+            s += '    {!s}\n'.format(problem_connection.problem_b.name)
         return s
 
 def decode_problems(problem_name, problem_data):
@@ -188,7 +256,7 @@ def decode(json_data_path):
     # todo: determine object type to be decoded, based on directory name
     entities = {
         'problems': {},
-        # 'connections': {}
+        'connections': {}
         }
 
     for problem_name, problem_data in data.items():
@@ -196,4 +264,5 @@ def decode(json_data_path):
         problem = Problem(name=problem_name, **problem_data)
 
     entities['problems'] = Problem._registry
+    entities['connections'] = ProblemConnection._registry
     return entities
