15ecaa2695368b0fcbd26af635976b151b88b143
Eli Chan <eli.chan@intertwine.io>
2016-08-24 07:33:00 -0500

    Align init for main with other blueprint modules

--- intertwine/main/__init__.py
+++ intertwine/main/__init__.py
@@ -4,11 +4,27 @@
 Main application for website.  Contains default/non-modified views
 '''
 from flask import Blueprint
+from alchy import Manager
+from alchy.model import extend_declarative_base
+
+from ..utils import BaseIntertwineModel
+
 
 modname = __name__.split('.')[-1]
 blueprint = Blueprint(modname, __name__, template_folder='templates',
                       static_folder='static')
 
+intertwine_db = Manager(Model=BaseIntertwineModel)
 
-# Must come later as we use blueprint in views
+# Attach query property to base model
+extend_declarative_base(intertwine_db, session=intertwine_db.session)
+
+# Must come later as we use blueprint and query property in views
 from . import views
+
+
+@blueprint.record_once
+def on_load(state):
+    # Sets up database tables
+    intertwine_db.config.update(state.app.config)
+    intertwine_db.create_all()
