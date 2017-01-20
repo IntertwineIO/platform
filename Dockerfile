84c4b8acf5777256eb80504189241276c4172ca8
Brian Bruggeman <Brian.M.Bruggeman@gmail.com>
2017-01-19 19:27:47 -0600

    Updates for docker

--- Dockerfile
+++ Dockerfile
@@ -4,17 +4,24 @@ FROM intertwineio/base
 # # ======================================================================
 COPY . /opt/repos/platform
 WORKDIR /opt/repos/platform
-ENV WORKON_HOME /root/.virtualenvs
+ENV WORKON_HOME /root/.envs
 
 # Install package
 RUN set -ex \
     && cd /opt/repos/platform \
     && vex -m --python python venv pip install --process-dependency-links -e .[all] \
     && vex -m --python python3 venv3 pip install --process-dependency-links -e .[all] \
-    # && vex -m --python pypy venvpy pip install --process-dependency-links -e .[all] \
+    && vex -m --python pypy venvpy pip install --process-dependency-links -e .[all] \
     && apt-get clean autoclean \
     && apt-get autoremove -y \
     && rm -Rf .eggs \
     && rm -rf /var/lib/{apt,dpkg,cache,log}/
 
-CMD ["vex", "venv", "./run.py", "-p", "8000", "-d"]
+CMD ["vex", "venv3", "./run.py", "-p", "8000", "-d"]
+
+ARG VERSION=Undefined
+LABEL org.label-schema.vendor="Intertwine.io" \
+      org.label-schema.url="https://intertwine.io" \
+      org.label-schema.name="Intertwine platform" \
+      org.label-schema.description="Untangling the World's Problems" \
+      org.label-schema.version=$VERSION
--- run_docker.py
+++ run_docker.py
@@ -7,10 +7,12 @@ Usage:
     run [options] [<command>]
 
 Options:
-    -h --help           This message
     -d --debug          Turn on debug
     -C --clean-docker   Clean docker images and containers
     -B --build          (Re)Builds docker image
+    -P --push           Pushes docker image
+    -R --no-run         Do not run
+    -i --image IMAGE    Image to use [default: intertwineio/platform]
 '''
 import os
 from logging import getLogger
@@ -21,28 +23,38 @@ logger = getLogger(__file__)
 
 
 def cleanup_docker():
-    logger.debug('Started: Cleaning up docker images and containers')
-    client = docker.from_env(assert_hostname=False)
+    # docker rm -v $(docker ps -a -q -f status=exited)
+    # docker rmi $(docker images -f "dangling=true" -q)
+    cmd = shlex.split('docker rm -v $(docker ps -a -q -f status=exited)')
+    process = subprocess.Popen(cmd)
+    process.wait()
+    # logger.debug('Started: Cleaning up docker images and containers')
+    # client = docker.from_env(assert_hostname=False)
     # Search for the exited containers and remove those
     # /bin/bash: docker rm $(docker ps -a -q)
-    removable_containers = {
-        container['Id'][:12]: container
-        for container in client.containers(all=True, filters={"status": "exited"})
-    }
-    for container_id, container in removable_containers.items():
-        client.remove_container(container_id)
+    # docker rm -v $(docker ps -a -q -f status=exited)
+    # removable_containers = {
+    #     container['Id'][:12]: container
+    #     for container in client.containers(all=True, filters={"status": "exited"})
+    # }
+    # for container_id, container in removable_containers.items():
+    #     client.remove_container(container_id)
 
     # Search for the images
-    # /bin/bash: docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
-    removable_images = {
-        image['Id'].split(':')[-1][:12]: image
-        for image in client.images()
-        if any('<none>' in tag for tag in image['RepoTags'])
-    }
-    for image_id, image in removable_images.items():
-        # Force doesn't always remove, but is needed or we error out
-        client.remove_image(image_id, force=True)
-    logger.debug('Finished: Cleaning up docker images and containers')
+    # docker rmi $(docker images -f "dangling=true" -q)
+    cmd = shlex.split('docker rmi $(docker images -f "dangling=true" -q)')
+    process = subprocess.Popen(cmd)
+    process.wait()
+    # removable_images = {
+    #     image['Id'].split(':')[-1][:12]: image
+    #     for image in client.images()
+
+    #     if any('<none>' in tag for tag in image['RepoTags'] if image['RepoTags'])
+    # }
+    # for image_id, image in removable_images.items():
+    #     # Force doesn't always remove, but is needed or we error out
+    #     client.remove_image(image_id, force=True)
+    # logger.debug('Finished: Cleaning up docker images and containers')
 
 
 def build_docker_image(tags=[]):
@@ -53,27 +65,40 @@ def push_docker_image():
     '''Pushes docker image to docker hub'''
 
 
-def main(**options):
-    client = docker.from_env(assert_hostname=False)
+def main(image, build=False, push=False, clean_docker=False, no_run=False, debug=False, command=None):
     local_repo_path = os.path.abspath(os.path.dirname(__file__))
+    # client = docker.AutoVersionClient()
+    client = docker.from_env(assert_hostname=False)
+    host_config = client.create_host_config(
+        binds={
+            local_repo_path: '/opt/repos/platform',
+        },
+        read_only=True,
+    )
 
-    if options.get('clean-docker'):
+    if clean_docker:
         cleanup_docker()
 
-    repo_name = os.path.dirname(__file__)
-    build_docker_image(tags=['intertwineio/{repo_name}'.format(repo_name)])
+    if build:
+        build_docker_image()
 
+    if push:
+        push_docker_image()
 
-    container = client.create_container(
-        image='platform',
-        command=command,
-        host_config=client.create_host_config(binds={
-            local_repo_path: '/opt/repos/platform',
-        })
+    if not no_run:
+        command = command or 'vex venv ./run.py -p 8000' + ' -d' if debug else ''
+        cmd = 'docker run -v .:/opt/repos/platform -it {image} {command}'
+        cmd = cmd.format(image=image, command=command)
+        process = subprocess.Popen(cmd)
+        process.wait()
+        # container = client.create_container(
+        #     image=options.get('image'),
+        #     command=command,
+        #     host_config=host_config,
 
-    )
-    client.start(container, publish_all_ports=True)
-
+        # )
+        # client.start(container, publish_all_ports=True)
+        # print(client.logs(container))
 
 
 if __name__ == '__main__':
@@ -83,5 +108,6 @@ if __name__ == '__main__':
         k.lstrip('-<').rstrip('>').replace('-', '_'): v
         for k, v in docopt(__doc__).items()
     }
+    print(options)
     main(**options)
 
