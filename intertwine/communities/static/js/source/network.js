a80e26f1b5ef725b4e8eaac9b47855244ef5d677
Eli Chan <eli.chan@intertwine.io>
2017-01-24 06:46:55 -0600

    Enable add for all connection categories

--- intertwine/communities/static/js/source/network.js
+++ intertwine/communities/static/js/source/network.js
@@ -10,85 +10,168 @@ $(document).ready(function() {
     var geoKey = community['geo'];
     var geo = geoKey ? payload[geoKey]: null;
 
-    var isAddingBroaderProblem = false;
+    var isAddingConnection = {'driver': false, 'impact': false, 'broader': false, 'narrower': false};
 
-    $('#add-broader-problem').on('click', function() {
-        if (isAddingBroaderProblem) {
+    function renderNewConnectionForm(category) {
+        var begHTML, adjacentProblemNameHTML, adjacentProblemIconHTML, endHTML;
+        var renderedConnectionForm;
+
+        if (category == 'driver' || category == 'impact') {
+            begHTML = '<div id="add-' + category + '-connection-component" class="v-problem">\n';
+            adjacentProblemNameHTML = (
+                '<div class="v-problem-link-container v-align-inner word-break">\n' +
+                    '<form id="add-' + category + '-connection-form" action="add-' + category + '-connection" method="post">\n' +
+                        '<input id="new-' + category + '-problem-name" type="text" name="new-' + category + '-problem-name" maxlength="60" placeholder="Problem Name" autofocus><br>\n' +
+                        '<a href=# id="submit-add-' + category + '-connection" class="submit-add-adjacent-connection-link"> Add </a>\n' +
+                        '<a href=# id="cancel-add-' + category + '-connection" class="cancel-add-adjacent-connection-link"> Cancel </a>\n' +
+                    '</form>\n' +
+                '</div>\n');
+            adjacentProblemIconHTML = (
+                '<div class="v-align-inner">\n' +
+                    '<i class="fa fa-circle problem-icon ' + category + '-icon"></i>\n' +
+                '</div>\n');
+            endHTML = '</div>\n';
+            renderedConnectionForm = (category == 'driver') ?
+                begHTML + adjacentProblemNameHTML + adjacentProblemIconHTML + endHTML :
+                begHTML + adjacentProblemIconHTML + adjacentProblemNameHTML + endHTML
+        } else {  // 'broader' or 'narrower'
+            begHTML = '<div id="add-' + category + '-connection-component" class="h-problem ' + category + '-problem">\n';
+            adjacentProblemNameHTML = (
+                '<div class="h-problem-link-container word-break">\n' +
+                    '<form id="add-' + category + '-connection-form" action="add-' + category + '-connection" method="post">\n' +
+                        '<input id="new-' + category + '-problem-name" type="text" name="new-' + category + '-problem-name" maxlength="60" placeholder="Problem Name" autofocus><br>\n' +
+                        '<a href=# id="submit-add-' + category + '-connection" class="submit-add-adjacent-connection-link"> Add </a>\n' +
+                        '<a href=# id="cancel-add-' + category + '-connection" class="cancel-add-adjacent-connection-link"> Cancel </a>\n' +
+                    '</form>\n' +
+                '</div>\n');
+            adjacentProblemIconHTML = '<i class="fa fa-circle problem-icon ' + category + '-icon"></i>\n';
+            endHTML = '</div>';
+            renderedConnectionForm = (category == 'broader') ?
+                begHTML + adjacentProblemNameHTML + adjacentProblemIconHTML + endHTML :
+                begHTML + adjacentProblemIconHTML + adjacentProblemNameHTML + endHTML
+        }
+
+        $('#' + category + '-scroll').prepend(renderedConnectionForm);
+    }
+
+
+    function renderNewRatedConnection(category, ratedConnectionPayload) {
+        var rootKey = ratedConnectionPayload.root_key;
+        var ratedConnection = ratedConnectionPayload[rootKey];
+        var begHTML, adjacentProblemNameHTML, adjacentProblemIconHTML, endHTML;
+        var renderedRatedConnection;
+
+        if (category == 'driver' || category == 'impact') {
+            begHTML = '<div class="v-problem">\n';
+            adjacentProblemNameHTML = (
+                '<div class="v-problem-link-container v-align-inner word-break">\n' +
+                    '<a href="' + ratedConnection.adjacent_community_uri + '" class="problem-link">' +
+                        ratedConnection.adjacent_problem_name +
+                        ' (' + ratedConnection.rating + ')\n' +
+                    '</a>\n' +
+                '</div>\n');
+            adjacentProblemIconHTML = (
+                '<div class="v-align-inner">\n' +
+                    '<a href="' + ratedConnection.adjacent_community_uri + '">\n' +
+                        '<i class="fa fa-circle problem-icon ' + category + '-icon"></i>\n' +
+                    '</a>\n' +
+                '</div>\n');
+            endHTML = '</div>\n';
+            renderedRatedConnection = (category == 'driver') ?
+                begHTML + adjacentProblemNameHTML + adjacentProblemIconHTML + endHTML :
+                begHTML + adjacentProblemIconHTML + adjacentProblemNameHTML + endHTML
+        } else {  // 'broader' or 'narrower'
+            begHTML = '<div class="h-problem ' + category + '-problem">\n';
+            adjacentProblemNameHTML = (
+                '<div class="h-problem-link-container word-break">\n' +
+                    '<a href="' + ratedConnection.adjacent_community_uri + '" class="problem-link">' +
+                        ratedConnection.adjacent_problem_name +
+                        ' (' + ratedConnection.rating + ')\n' +
+                    '</a>\n' +
+                '</div>\n');
+            adjacentProblemIconHTML = (
+                '<a href="' + ratedConnection.adjacent_community_uri + '">\n' +
+                    '<i class="fa fa-circle problem-icon ' + category + '-icon"></i>\n' +
+                '</a>\n');
+            endHTML = '</div>\n';
+            renderedRatedConnection = (category == 'broader') ?
+                begHTML + adjacentProblemNameHTML + adjacentProblemIconHTML + endHTML :
+                begHTML + adjacentProblemIconHTML + adjacentProblemNameHTML + endHTML
+        }
+
+        $('#' + category + '-scroll').append(renderedRatedConnection);
+    }
+
+    function submitAddConnection(category) {
+        var newProblemName = $('#new-' + category + '-problem-name').val();
+        var axis = (category == 'driver' || category == 'impact') ? 'causal' : 'scoped';
+        var problemA = (category == 'driver' || category == 'broader') ? newProblemName : problem['name'];
+        var problemB = (category == 'driver' || category == 'broader') ? problem['name'] : newProblemName;
+
+        var addConnectionPayload = {
+            "connection": {
+                "axis": axis,
+                "problem_a": problemA,
+                "problem_b": problemB
+            },
+            "community": {
+                "problem": problem['human_id'],
+                "org": null,
+                "geo": geo['human_id']
+            },
+            "aggregation": "strict"
+        };
+
+        $.ajax({
+            type: "POST",
+            url: "http://localhost:5000/problems/rated_connections",
+            data: JSON.stringify(addConnectionPayload),
+            dataType: "json",
+            contentType : "application/json",
+            success: function(ratedConnectionPayload){
+                renderNewRatedConnection(category, ratedConnectionPayload);
+            } // TODO: Add new connection and feedback message
+        });
+
+        $('#add-' + category + '-connection-component').remove();
+        isAddingConnection[category] = false;
+    }
+
+    function cancelAddConnection(category) {
+        $('#add-' + category + '-connection-component').remove();
+        isAddingConnection[category] = false;
+    }
+
+    function showNewConnectionForm(category) {
+        if (isAddingConnection[category]) {
             return;
         }
-        isAddingBroaderProblem = true;
+        isAddingConnection[category] = true;
 
-        $('#broader-scroll').prepend(  // TODO: move to AddBroaderProblem component
-            '<div id="add-broader-problem-component" class="h-problem broader-problem">\n' +
-                '<div class="h-problem-link-container word-break">\n' +
-                    '<form id="add-broader-problem-form" action="add-broader-problem" method="post">\n' +
-                        '<input id="add-broader-problem-name" type="text" name="add-broader-problem-name" maxlength="60" placeholder="Broader Problem" autofocus><br>\n' +
-                        '<a href=# id="submit-add-broader-problem" class="submit-add-adjacent-problem-link"> Add </a>\n' +
-                        '<a href=# id="cancel-add-broader-problem" class="cancel-add-adjacent-problem-link"> Cancel </a>\n' +
-                    '</form>\n' +
-                '</div>\n' +
-                '<i class="fa fa-circle problem-icon broader-icon"></i>\n' +
-            '</div>'
-        );
+        renderNewConnectionForm(category);
 
-        function renderNewBroaderConnection(ratedConnectionPayload) {
-            console.log(ratedConnectionPayload);
-            var rootKey = ratedConnectionPayload.root_key;
-            var ratedConnection = ratedConnectionPayload[rootKey];
-
-            $('#broader-scroll').append(
-                '<div class="h-problem broader-problem">' +
-                    '<div class="h-problem-link-container word-break">' +
-                        '<a href="' + ratedConnection.adjacent_community_uri + '" class="problem-link">' +
-                            ratedConnection.adjacent_problem_name +
-                            ' (' + ratedConnection.rating + ')' +
-                        '</a>' +
-                    '</div>' +
-                    '<a href="' + ratedConnection.adjacent_community_uri + '">' +
-                        '<i class="fa fa-circle problem-icon broader-icon"></i>' +
-                    '</a>' +
-                '</div>'
-            );
-        }
-
-        $('#submit-add-broader-problem').on('click', function() {
-            var problemA = $('#add-broader-problem-name').val();
-
-            var addBroaderProblemPayload = {
-                "connection": {
-                    "axis": "scoped",
-                    "problem_a": problemA,
-                    "problem_b": problem['name']
-                },
-                "community": {
-                    "problem": problem['human_id'],
-                    "org": null,
-                    "geo": geo['human_id']
-                },
-                "aggregation": "strict",
-                "rating": -1,
-                "weight": 0
-            };
-            console.log(addBroaderProblemPayload);
-
-            $.ajax({
-                type: "POST",
-                url: "http://localhost:5000/problems/rated_connections",
-                data: JSON.stringify(addBroaderProblemPayload),
-                dataType: "json",
-                contentType : "application/json",
-                success: function(ratedConnectionPayload){
-                    renderNewBroaderConnection(ratedConnectionPayload);
-                } // Add new connection and feedback message
-            });
-
-            $('#add-broader-problem-component').remove()
-            isAddingBroaderProblem = false;
+        $('#submit-add-' + category + '-connection').on('click', function() {
+            submitAddConnection(category);
         });
 
-        $('#cancel-add-broader-problem').on('click', function() {
-            $('#add-broader-problem-component').remove()
-            isAddingBroaderProblem = false;
+        $('#cancel-add-' + category + '-connection').on('click', function() {
+            cancelAddConnection(category);
         });
+    }
+
+    $('#add-driver-connection').on('click', function() {
+        showNewConnectionForm('driver');
+    });
+
+    $('#add-impact-connection').on('click', function() {
+        showNewConnectionForm('impact');
+    });
+
+    $('#add-broader-connection').on('click', function() {
+        showNewConnectionForm('broader');
+    });
+
+    $('#add-narrower-connection').on('click', function() {
+        showNewConnectionForm('narrower');
     });
 });
