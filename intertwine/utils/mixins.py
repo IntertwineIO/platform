da2f9f8421c69e2ab1e15198ecf1b62fd08d37a8
Eli Chan <eli.chan@intertwine.io>
2017-10-06 07:52:03 -0500

    Add namedtuple support to Jsonable.jsonify()

--- intertwine/utils/mixins.py
+++ intertwine/utils/mixins.py
@@ -10,6 +10,7 @@ from itertools import chain, islice
 from math import floor
 from mock.mock import NonCallableMagicMock
 from operator import attrgetter, itemgetter
+from past.builtins import basestring
 
 import pendulum
 from sqlalchemy import Column, orm, types
@@ -380,10 +381,16 @@ class Jsonable(object):
                 continue
 
             try:
-                if isinstance(value, (str, unicode)):
+                value._asdict()  # Raise AttributeError if not namedtuple
+                self_json[field] = value  # json.dumps supports namedtuples
+                continue
+            except AttributeError:
+                pass
+
+            try:
+                if isinstance(value, basestring):
                     raise TypeError
-                # Raise TypeError if not iterable
-                value_iterator = iter(value)
+                value_iterator = iter(value)  # Raise TypeError if not iterable
 
             except TypeError:
                 self_json[field] = default(value)
