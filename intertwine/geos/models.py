22789f76e405e6735a0ec882c5e9a614f5214d54
Eli Chan <eli.chan@intertwine.io>
2017-10-17 18:44:12 -0500

    Move geo.utils to utils.space to consolidate utils

--- intertwine/geos/models.py
+++ intertwine/geos/models.py
@@ -11,8 +11,8 @@ from sqlalchemy.orm.collections import attribute_mapped_collection
 
 from intertwine import IntertwineModel
 from intertwine.exceptions import (AttributeConflict, CircularReference)
-from intertwine.geos.utils import Area, Coordinate, GeoLocation
 from intertwine.utils.mixins import JsonProperty
+from intertwine.utils.space import Area, Coordinate, GeoLocation
 from intertwine.utils.tools import (define_constants_at_module_scope,
                                     find_any_words)
 
