e3bff3988c7d330f60c9f14fe22f28266a8854e5
Eli Chan <eli.chan@intertwine.io>
2017-10-22 11:08:02 -0500

    Fix import of Enum

--- intertwine/utils/enums.py
+++ intertwine/utils/enums.py
@@ -3,10 +3,7 @@
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
-import sys
-
-if sys.version_info < (3, 4):
-    from enum import Enum
+from enum import Enum
 
 
 MatchType = Enum('FilterType', 'BEST, EXACT, STARTS_WITH, ENDS_WITH, CONTAINS',
