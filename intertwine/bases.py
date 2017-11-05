3f6fb9d83b530e5d0e458a5043b110dd65e2c5b0
Eli Chan <eli.chan@intertwine.io>
2017-11-05 03:55:58 -0600

    Move _class_init_ into InitiationMixin/InitiationMetaMixin

--- intertwine/bases.py
+++ intertwine/bases.py
@@ -4,21 +4,16 @@ from __future__ import (absolute_import, division, print_function,
 
 from alchy.model import ModelBase
 
+from intertwine.initiation import InitiationMixin, InitiationMetaMixin
 from intertwine.trackable import Trackable
 from intertwine.utils.jsonable import Jsonable
 from intertwine.utils.mixins import AutoTableMixin
 
 
-class BaseIntertwineMeta(Trackable):
-
-    def __init__(cls, name, bases, attr):
-        super(BaseIntertwineMeta, cls).__init__(name, bases, attr)
-        cls._class_init_()
+class BaseIntertwineMeta(InitiationMetaMixin, Trackable):
+    pass
 
 
-class BaseIntertwineModel(Jsonable, AutoTableMixin, ModelBase):
-
-    @classmethod
-    def _class_init_(cls):
-        # the delegation chain stops here
-        assert not hasattr(super(BaseIntertwineModel, cls), '_class_init_')
+class BaseIntertwineModel(InitiationMixin, Jsonable, AutoTableMixin,
+                          ModelBase):
+    pass
