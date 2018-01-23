1365003061f86c4ba2e7439dfd32e533a7bae3f7
Jason M Harrison <jason.m.harrison@gmail.com>
2018-01-22 21:32:30 -0600

    Scaffolded front end w/ webpack including example component.

---
+++ intertwine_fe/src/components/example.vue
@@ -0,0 +1,37 @@
+<template>
+  <main class="hello">
+    <h1>{{ msg }}</h1>
+    
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Example Component',
+  data() {
+    return {
+      msg: 'Vue is up and running within Intertwine',
+    };
+  },
+};
+</script>
+
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #42b983;
+  }
+</style>
