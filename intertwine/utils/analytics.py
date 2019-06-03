cbb862cacf8991801a9d039004494c96ab8d8d58
Eli Chan <eli.chan@intertwine.io>
2019-06-03 18:36:30 -0500

    Refine average util to support iterables and optional weights

--- intertwine/utils/analytics.py
+++ intertwine/utils/analytics.py
@@ -3,7 +3,31 @@
 from itertools import zip_longest
 
 
-def weighted_average(values, weights):
-    numerator = sum(value * weight for (value, weight) in zip_longest(values, weights))
-    denominator = sum(weights)
+def average(values, weights=None):
+    """
+    Average
+
+    I/O:
+    values:         iterable of numeric values
+    weights=None:   iterable of numeric weights to apply to values
+    return:         average of values, optionally weighted
+    raise:          ValueError if not same number of values and weights
+    raise:          ZeroDivisionError if weights sum to 0
+    raise:          TypeError if any value or weight is non-numeric
+    """
+    numerator = denominator = 0
+
+    if weights is None:
+        for denominator, value in enumerate(values, start=1):
+            numerator += value
+        return numerator / denominator
+
+    try:
+        for value, weight in zip_longest(values, weights):
+            numerator += value * weight
+            denominator += weight
+    except TypeError as e:
+        if len(values) != len(weights):
+            raise ValueError('There must be the same number of values and weights') from e
+        raise
     return numerator / denominator
--- tests/utils/test_analytics.py
+++ tests/utils/test_analytics.py
@@ -1,29 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import pytest
+from itertools import tee
 
-from intertwine.utils.analytics import weighted_average
+from intertwine.utils.analytics import average
+from intertwine.utils.duck_typing import isiterator
 from intertwine.utils.tools import is_child_class
 
 
 @pytest.mark.unit
 @pytest.mark.parametrize(
-    ('idx', 'values',           'weights',                 'check'),
+    ('idx', 'values',                   'weights',                 'check'),
     [
-     (0,    [0, 1],             [1, 0],                    0),
-     (1,    [1, 2],             [1, 0],                    1),
-     (2,    [1, 2],             [0, 2],                    2),
-     (3,    [1, 4],             [1, 2],                    3),
-     (4,    [2, 8],             [2, 1],                    4),
-     (5,    [3, 6, 9],          [2, 0, 1],                 5),
-     (6,    [3, 6, 9],          [1, 1, 1],                 6),
-     (7,    [3, 6, 9],          [0, 2, 1],                 7),
-     (8,    [1, 2, 3],          [1, 1],                    TypeError),
-     (9,    [1, 2],             [1, 1, 1],                 TypeError),
+     (0,    [0, 1],                     [1, 0],                    0),
+     (1,    [1, 2],                     [1, 0],                    1),
+     (2,    [1, 2],                     [0, 1],                    2),
+     (3,    [1, 4],                     [1, 2],                    3),
+     (4,    [2, 3, 5],                  [1, 2, 4],                 4),
+     (5,    [3, 5, 7],                  None,                      5),
+     (6,    (i * 6 for i in range(3)),  None,                      6),
+     (7,    (i * 7 for i in range(3)),  [1, 1, 1],                 7),
+     (8,    (i * 8 for i in range(3)),  (i for i in [1] * 3),      8),
+     (9,    [6, 9, 12],                 (i for i in [1] * 3),      9),
+     (10,   [1, 2, 3],                  [1, 1],                    ValueError),
+     (11,   [1, 2],                     [1, 1, 1],                 ValueError),
+     (12,   [1, 2],                     [0],                       ValueError),
+     (13,   [1, 2],                     ['a'],                     ValueError),
+     (14,   [1, 2],                     [0, 0],                    ZeroDivisionError),
+     (15,   [],                         [],                        ZeroDivisionError),
+     (16,   [1, 2],                     [1, 'a'],                  TypeError),
+     (17,   [1, 'a'],                   [1, 1],                    TypeError),
+     (18,   None,                       [1, 1],                    TypeError),
+     (19,   None,                       None,                      TypeError),
      ])
-def test_weighted_average(idx, values, weights, check):
+def test_average(idx, values, weights, check):
     if is_child_class(check, Exception):
         with pytest.raises(check):
-            weighted_average(values, weights)
+            average(values, weights)
     else:
-        weighted_average(values, weights)
+        if weights is None:
+            values, values2 = tee(values, 2) if isiterator(values) else (values, values)
+            assert average(values2) == check
+        assert average(values, weights) == check
